f1179ac5ac8083f01ef7693cc88148c58b9a3bf5
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-03-25 07:48:52 +0600

    initial commit

---
+++ Week 4/colormod.h
@@ -0,0 +1,33 @@
+#include <ostream>
+namespace Color {
+    enum Code {
+        FG_RED      = 31,
+        FG_GREEN    = 32,
+        FG_YELLOW   = 33,
+        FG_BLUE     = 34,
+        FG_MAGENTA  = 35,        
+        FG_CYAN		= 36,        
+        FG_DEFAULT  = 39,
+        
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+    class Modifier {
+        Code code;
+    public:
+        Modifier(Code pCode) : code(pCode) {}
+        friend std::ostream&
+        operator<<(std::ostream& os, const Modifier& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+}
+Color::Modifier red(Color::FG_RED);
+Color::Modifier green(Color::FG_GREEN);
+Color::Modifier yellow(Color::FG_YELLOW);
+Color::Modifier blue(Color::FG_BLUE);
+Color::Modifier magenta(Color::FG_MAGENTA);
+Color::Modifier cyan(Color::FG_CYAN);
+Color::Modifier def(Color::FG_DEFAULT);
