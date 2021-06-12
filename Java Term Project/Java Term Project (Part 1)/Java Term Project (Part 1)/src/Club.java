669bf7d5b084f8f7c56a572bc2880e00534c9c85
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-06-12 11:07:34 +0600

    .

--- Java Term Project/Java Term Project (Part 1)/Java Term Project (Part 1)/src/Club.java
+++ Java Term Project/Java Term Project (Part 1)/Java Term Project (Part 1)/src/Club.java
@@ -1,6 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Club {
-    double MAXIMUM_SALARY;
-    int MAXIMUM_AGE;
-    double MAXIMUM_HEIGHT;
-    double TOTAL_SALARY;
+    private int ID;
+    private double MAXIMUM_SALARY;
+    private int MAXIMUM_AGE;
+    private double MAXIMUM_HEIGHT;
+    private double TOTAL_SALARY;
+    private List<Player> players;
+
+    public Player SearchPlayerByName(String name){
+        for (var player:players){
+            if (player.getName().equalsIgnoreCase(name)){
+                return player;
+            }
+        }
+        return null;
+    }
+
+    public List<Player> SearchPlayerByClubCountry(String club, String country){
+        List<Player>wantedPlayers = new ArrayList<>();
+        for (var player:players){
+            if (player.getCountry().equalsIgnoreCase(country) && (club.equalsIgnoreCase("ANY") || player.getClub().equalsIgnoreCase(club))){
+                wantedPlayers.add(player);
+            }
+        }
+        return wantedPlayers;
+    }
+
+    public List<Player> SearchPlayerByPosition(String position){
+        List<Player>wantedPlayers = new ArrayList<>();
+        for (var player:players){
+            if (player.getPosition().equalsIgnoreCase(position)){
+                wantedPlayers.add(player);
+            }
+        }
+        return wantedPlayers;
+    }
+
+    public List<Player> SearchPlayerBySalary(double lowRange, double highRange){
+        List<Player>wantedPlayers = new ArrayList<>();
+        for (var player:players){
+            double salary = player.getSalary();
+            if (lowRange <= salary && salary <= highRange){
+                wantedPlayers.add(player);
+            }
+        }
+        return wantedPlayers;
+    }
+
+    public List<Player> SearchMaximumSalary(){
+        List<Player>wantedPlayers = new ArrayList<>();
+        for (var player:players){
+            if (player.getSalary() == MAXIMUM_SALARY){
+                wantedPlayers.add(player);
+            }
+        }
+        return wantedPlayers;
+    }
+
+    public List<Player> SearchMaximumAge(){
+        List<Player>wantedPlayers = new ArrayList<>();
+        for (var player:players){
+            if (player.getAge() == MAXIMUM_AGE){
+                wantedPlayers.add(player);
+            }
+        }
+        return wantedPlayers;
+    }
+
+    public List<Player> SearchMaximumHeight(){
+        List<Player>wantedPlayers = new ArrayList<>();
+        for (var player:players){
+            if (player.getHeight() == MAXIMUM_HEIGHT){
+                wantedPlayers.add(player);
+            }
+        }
+        return wantedPlayers;
+    }
+
+    public double TotalSalary(){
+        return TOTAL_SALARY;
+    }
+
+    public void addPlayer(Player p){
+
+    }
 }
---
+++ Java Term Project/Java Term Project (Part 1)/Java Term Project (Part 1)/src/Country.java
@@ -0,0 +1,29 @@
+public class Country {
+    int ID;
+    String name;
+    int count;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
