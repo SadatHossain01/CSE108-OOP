868f2e4e9a7561fc998db5dbc86573bec6db70d2
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-06-12 10:26:16 +0600

    initial commit

---
+++ Java Term Project/Java Term Project (Part 1)/Java Term Project (Part 1)/src/Club.java
@@ -0,0 +1,6 @@
+public class Club {
+    double MAXIMUM_SALARY;
+    int MAXIMUM_AGE;
+    double MAXIMUM_HEIGHT;
+    double TOTAL_SALARY;
+}
---
+++ Java Term Project/Java Term Project (Part 1)/Java Term Project (Part 1)/src/Player.java
@@ -0,0 +1,80 @@
+public class Player {
+    private String name, country, club, position;
+    private int age, number;
+    private double height, salary;
+
+    public Player(String name, String country, int age, double height, String club, String position, int number, double salary){
+        this.name = name;
+        this.country = country;
+        this.age = age;
+        this.height = height;
+        this.club = club;
+        this.position = position;
+        this.number = number;
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
