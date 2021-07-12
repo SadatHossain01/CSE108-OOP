9cbde0c3654ec2691145dca87c3fa54c53009cca
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-07-12 23:55:26 +0600

    initialized jfoenix

--- Java Term Project/Part 2/src/module-info.java
+++ Java Term Project/Part 2/src/module-info.java
@@ -2,5 +2,6 @@ module myjfx {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires com.jfoenix;
     opens sample to javafx.graphics, javafx.fxml, javafx.base;
 }
