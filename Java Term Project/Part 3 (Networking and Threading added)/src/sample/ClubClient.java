288051efdf6c98c1eb012714ac566f5a895f35de
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-07-18 02:21:49 +0600

    .

--- Java Term Project/Part 3 (Networking and Threading added)/src/sample/ClubClient.java
+++ Java Term Project/Part 3 (Networking and Threading added)/src/sample/ClubClient.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class ClubClient extends Application {
+    public Stage primaryStage;
+    public static double screenHeight, screenWidth;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
