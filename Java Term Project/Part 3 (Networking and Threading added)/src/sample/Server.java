08cf8c264eb57f90f5d917d2a361886b831397eb
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-07-17 21:43:59 +0600

    .

--- Java Term Project/Part 3 (Networking and Threading added)/src/sample/Server.java
+++ Java Term Project/Part 3 (Networking and Threading added)/src/sample/Server.java
@@ -1,10 +1,26 @@
 package sample;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    ServerSocket
-    public static void main(String[] args) {
+    ServerSocket serverSocket;
 
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void serve(Socket clientSocket){
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = 44444;
+        Server server = new Server(port);
+        while (true){
+            var cs = server.serverSocket.accept();
+            server.serve(cs);
+        }
     }
 }
